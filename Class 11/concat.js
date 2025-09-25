cb3f14611783728adaa84bf60e91d8f85dd165cc
devasish <devasish@mishra.pro>
2025-09-25 10:33:48 +0530

    update

---
+++ Class 11/concat.js
@@ -0,0 +1,20 @@
+console.log("10" - 2);  // 9
+console.log("10" * "2");  // 20
+console.log("10" / "2");  // 5
+
+
+console.log("apples" + 45);   // apples45
+let res = "apples" * 45;
+console.log(typeof res);   // NaN
+
+console.log(45/3 + 2+10 * 8 - (2*3));  // 91
+console.log("abc" - "bc");
+
+for (let i = 1; i <= 20; i++){
+    if (i % 2== 0){
+        console.log(i);
+    }
+}
+
+
+
---
+++ Class 11/script.js
@@ -0,0 +1,60 @@
+console.log('hello from VSCode')
+
+// print sum fo 100 and 200
+console.log(100+200)
+
+//variables
+var username = 'Priyanshi'
+console.log(username)
+console.log(typeof username)
+
+var age = 65
+console.log(age)
+console.log(typeof age)
+
+var age = 25
+console.log(age)
+
+
+let num = 20;
+console.log(num)
+
+// let num = 100 // Error: Identifier 'num' has already been declared
+// console.log(num)
+
+
+const pi = 3.14;
+console.log(typeof pi)
+// const pi = 6.3 // Redeclaration : not allowed
+// pi = 3.1415 // Reassignment : not allowed 
+
+
+let psp;
+// const g; // Error: Missing initializer in const declaration 
+
+const g = 9.8;
+psp = 65.34;
+console.log(g);
+console.log(psp);
+
+let ispresent = true;
+console.log(ispresent);
+console.log(typeof ispresent);
+
+let nothing = null;
+console.log(typeof nothing);
+
+
+console.log("hello" + "world");
+console.log(9 + "hello");
+console.log(15 + 20);
+console.log("15" + "30");
+console.log("35" - "15");
+
+
+
+ 
+
+
+
+
