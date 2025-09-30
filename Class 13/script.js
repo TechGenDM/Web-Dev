2ceb2c783caff0f9049e9f1df9a122f8076cf333
devasish <devasish@mishra.pro>
2025-09-30 14:15:11 +0530

    update

---
+++ Class 13/script.js
@@ -0,0 +1,56 @@
+// let arr = [10, 'Priyanshi', true, 25, -9.5, 25];
+
+// console.log(arr.indexOf(25));    // 3
+// console.log(arr.indexOf(250));    // -1
+// console.log(arr.indexOf(true));    // 2
+
+// console.log(arr.includes("Priyanshi"));   // true
+// console.log(arr.includes("priyanshi"));   // false
+
+// let arr1 = [1, "Priyanshi", -999];
+// let arr2 = [true, 1.011, "HelloMoto143"];     
+// console.log(arr1.concat(arr2));
+
+
+
+// let str = 'My name is Priyanshi'
+// let arr = str.split(" ")
+// console.log(arr) // ['My', 'name', 'is', 'Priyanshi']
+
+// let arr2 = str.split("i")
+// console.log(arr2) // ['My name ', 's Pr', 'yansh', '']
+
+// let str2 = 'Hello'
+// let arr3 = str2.split("")
+// console.log(arr3) // ['H', 'e', 'l', 'l', 'o']
+
+// arr3.reverse()
+// console.log(arr3) // ['o', 'l', 'l', 'e', 'H']
+
+// let reversedStr = arr3.join('')
+// console.log(reversedStr) // olleH 
+
+// let joinedStr = arr3.join();
+// console.log(joinedStr) // o,l,l,e,H
+// let joinedStr1 = arr3.join(' ');
+// console.log(joinedStr1) //o l l e H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Class 13/slice.js
@@ -0,0 +1,46 @@
+// let fruits = ['apple', 'kiwi', 'mango', 'melon', 'banana']
+// console.log(fruits);
+
+// remove elements
+// let removeedElements = fruits.splice(1, 3)
+// console.log(removeedElements);  //  ['kiwi', 'mango', 'melon']
+// console.log(fruits);            // ['apple', 'banana']
+// let removedElements = fruits.splice(2, 2);
+// console.log(removedElements);  //  ['mango', 'melon']
+// console.log(fruits);            //  ['apple', 'kiwi', 'banana']
+// let x = fruits.splice(2, 0, 'Watermelon');
+// console.log(x);   // []
+// console.log(fruits)   // ['apple', 'kiwi', 'Watermelon', 'banana']
+// let y = fruits.splice(2, 1, 'peach', 'litchi');
+// console.log(y);    // []
+// console.log(fruits);     // ['apple', 'kiwi', 'peach', 'Watermelon', 'banana']
+
+
+
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+// let arr2 = arr.slice(4)
+// console.log(arr2);  // [50, 60, 70, 80, 90, 100]
+// let arr3 = arr.slice(3, 7)
+// console.log(arr3);   // [40, 50, 60, 70]
+// let arr4= arr.slice(-2)
+// console.log(arr4);   //  [90, 100]
+// let arr5 = arr.slice(-4, -1)
+// console.log(arr5);   // [70, 80, 90]
+// let arr6 = arr.slice()
+// console.log(arr6);
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
